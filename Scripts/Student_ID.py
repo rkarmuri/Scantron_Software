8bca273bb2e5c8d0008a35d61c953ae46e41c35b
Joseph G <69803859+Annoyed-Raven@users.noreply.github.com>
2023-04-04 16:00:28 -0500

    adding scripts for image processing

---
+++ Scripts/Student_ID.py
@@ -0,0 +1,19 @@
+# Import packages
+import cv2
+import numpy as np
+ 
+img = cv2.imread('downloaded_images\scantron-page0.jpg')
+print(img.shape) # Print image shape
+cv2.imshow("original", img)
+ 
+# Cropping an image
+cropped_image = img[1275:1815, 1000:1520]
+ 
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+ 
+# Save the cropped image
+cv2.imwrite("Student Id.jpg", cropped_image)
+ 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
